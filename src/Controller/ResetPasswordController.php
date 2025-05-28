bb081f846cfbeb2f90bb1bfb3db15305942657e7
ThomasdevLC <thomas.lecam@gmail.com>
2025-05-28 14:56:56 +0200

    front redirection after reset

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -125,7 +125,8 @@ class ResetPasswordController extends AbstractController
             // The session is cleaned up after the password has been changed.
             $this->cleanSessionAfterReset();
 
-            return $this->redirectToRoute('app_home');
+            $frontendUrl = $_ENV['FRONT_URL'];
+            return $this->redirect($frontendUrl . '/login?reset=1');
         }
 
         return $this->render('reset_password/reset.html.twig', [
