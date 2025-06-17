25ee24081306596e590f83fcadbbc5dd5d5fedfe
ThomasdevLC <thomas.lecam@gmail.com>
2025-06-17 17:04:25 +0200

    migration + fixtures script

--- migrations/Version20250617134947.php
+++ migrations/Version20250617134947.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250612150114 extends AbstractMigration
+final class Version20250617134947 extends AbstractMigration
 {
     public function getDescription(): string
     {
