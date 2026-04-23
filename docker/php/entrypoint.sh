45699897a6fd3603b33417779c3ff2015a9ba586
Thomas <thomas.lecam@gmail.com>
2026-04-23 18:29:47 +0200

    fix:  entrypoint + docherfile

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -7,9 +7,11 @@ if [ "$APP_ENV" = "prod" ]; then
     mkdir -p var/cache var/log var/sessions
 
     php bin/console lexik:jwt:generate-keypair --skip-if-exists --env=prod
+    php bin/console importmap:install --env=prod
+    php bin/console asset-map:compile --env=prod
     php bin/console cache:warmup --env=prod
 
-    chown -R www-data:www-data var config/jwt
+    chown -R www-data:www-data var config/jwt public/assets
     chmod -R 775 var config/jwt
 
     nginx -g "daemon off;" &
