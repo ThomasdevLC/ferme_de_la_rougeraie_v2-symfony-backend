16d9d545c6bc920e22c96657230a3e121dd17a74
Thomas <thomas.lecam@gmail.com>
2026-07-11 13:59:58 +0200

    feat(api): expose product variants in the products endpoint

--- src/Mapper/ProductMapper.php
+++ src/Mapper/ProductMapper.php
@@ -3,17 +3,26 @@
 namespace App\Mapper;
 
 use App\Dto\Product\ProductDto;
+use App\Dto\Product\ProductVariantDto;
 use App\Entity\Product;
+use App\Entity\ProductVariant;
 use App\Utils\Translator\UnitTranslator;
 
 class ProductMapper
 {
     public static function toDto(Product $product, UnitTranslator $translator): ProductDto
     {
+        $variants = [];
+        foreach ($product->getVariants() as $variant) {
+            if ($variant->isDisplayed()) {
+                $variants[] = self::variantToDto($variant);
+            }
+        }
+
         return new ProductDto(
             id: $product->getId(),
             name: $product->getName(),
-            price: $product->getPriceInEuros(),
+            price: $product->hasVariants() ? null : $product->getPriceInEuros(),
             unit: $translator->translate($product->getUnit()),
             image: '/uploads/images/' . $product->getImage(),
             hasStock:    $product->hasStock(),
@@ -22,6 +31,18 @@ class ProductMapper
             discount: $product->isDiscount(),
             discountText: $product->getDiscountText(),
             inter: $product->getInter(),
+            hasVariants: $product->hasVariants(),
+            variants: $variants,
+        );
+    }
+
+    private static function variantToDto(ProductVariant $variant): ProductVariantDto
+    {
+        return new ProductVariantDto(
+            id: $variant->getId(),
+            label: $variant->getLabel(),
+            price: $variant->getPriceInEuros(),
+            stock: $variant->getStock(),
         );
     }
 }
