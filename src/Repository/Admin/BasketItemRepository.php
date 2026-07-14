977a47d2fed0460f035433cc2db542e99fb13ba2
Thomas <thomas.lecam@gmail.com>
2026-07-14 14:51:16 +0200

    feat(basket): add is_basket flag and basket_item composition entity

---
+++ src/Repository/Admin/BasketItemRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repository\Admin;
+
+use App\Entity\BasketItem;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<BasketItem>
+ */
+class BasketItemRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, BasketItem::class);
+    }
+}
